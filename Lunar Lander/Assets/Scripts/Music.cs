bd9f31493b051914e92116fe6e5c1b94cd4246c0
BraydenMiller <48822485+BraydenMiller@users.noreply.github.com>
2019-06-27 14:05:26 +0800

    Music
    -TitleScreen music

---
+++ Lunar Lander/Assets/Scripts/Music.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    //A reference to our audiosource & audioclip.
+    AudioSource myAudioSource;
+
+    public AudioClip myAudioClip;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        MusicMaker();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Handles our game music.
+    void MusicMaker()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+        myAudioSource.clip = myAudioClip;
+        myAudioSource.Play();
+    }
+}
