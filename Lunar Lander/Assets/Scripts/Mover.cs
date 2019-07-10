ca129107ca0d611839427f00443f810e92f5b16b
BraydenMiller <48822485+BraydenMiller@users.noreply.github.com>
2019-07-10 21:12:27 +0800

    Stuff Main Title
    asdasdasdasdasd

--- Lunar Lander/Assets/Scripts/Mover.cs
+++ Lunar Lander/Assets/Scripts/Mover.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Mover : MonoBehaviour
 {
 
@@ -13,17 +14,21 @@ public class Mover : MonoBehaviour
     public float deadZone;
     public float gravity;
     public bool isGrounded;
+    public bool letPlay = true;
     public float turnRate;
 
     public GameObject engineObject;
-    public GameObject downwardsParticles;
-    public GameObject horizontalParticles;
+    public ParticleSystem downParticle1;
+    public ParticleSystem downParticle2;
+    public ParticleSystem downParticle3;
+    public ParticleSystem downParticle4;
+
     public GameObject landingObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -35,9 +40,11 @@ public class Mover : MonoBehaviour
         //Handles verticle movement using thrust & gravity with controller.
         if (Input.GetAxis("Upwards") > deadZone)
         {
+            letPlay = true;
             myRigidBody.AddForce(transform.up * thrust);
             isGrounded = false;
             TurnOffLanding();
+            downParticle1.Play();
             ShipEngines();
         }
         //Handles verticle movement using thrust & gravity with keyboard.
@@ -45,18 +52,42 @@ public class Mover : MonoBehaviour
         {
             myRigidBody.AddForce(transform.up * thrust);
             isGrounded = false;
+            letPlay = true;
             TurnOffLanding();
             ShipEngines();
         }
         else
         {
+            letPlay = false;
             myRigidBody.AddForce(transform.up * gravity);
             TurnOffEngines();
         }
 
-        //Handles moving left, right, forward and backwards using our sideThrust value.
-        //XboxController
-        if (Input.GetAxis("Forwards") < -deadZone && isGrounded == false)
+        if (letPlay)
+        {
+            if (!downParticle1.isPlaying)
+            {
+                downParticle1.Play();
+                downParticle2.Play();
+                downParticle3.Play();
+                downParticle4.Play();
+            }
+        }
+        else
+        {
+            if (downParticle1.isPlaying)
+            {
+                downParticle1.Stop();
+                downParticle2.Stop();
+                downParticle3.Stop();
+                downParticle4.Stop();
+            }
+
+        }
+
+            //Handles moving left, right, forward and backwards using our sideThrust value.
+            //XboxController
+            if (Input.GetAxis("Forwards") < -deadZone && isGrounded == false)
         {
             myRigidBody.AddForce(transform.forward * thrust * sideThrusters);
         }
@@ -102,14 +133,12 @@ public class Mover : MonoBehaviour
     void ShipEngines()
     {
         engineObject.SetActive(true);
-        downwardsParticles.SetActive(true);
     }
 
     //Turns our engine object off.
     void TurnOffEngines()
     {
         engineObject.SetActive(false);
-        downwardsParticles.SetActive(false);
     }
 
     //Turns our landing object off.
